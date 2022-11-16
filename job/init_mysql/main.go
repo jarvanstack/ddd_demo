e132c531b097367f6d01d358b2bd30ebd27e76d4
dengjiawen <dengjiawen@bilibili.com>
2022-11-16 14:47:46 +0800

    feat: 优化 make init 在没有数据库的时候自动创建数据库

--- job/init_mysql/main.go
+++ job/init_mysql/main.go
@@ -16,35 +16,82 @@ import (
 )
 
 const (
-	sqlPath    = "./ddd_demo.sql"
-	configPath = "../../config.yaml"
+	sqlPath         = "../../sql/ddd_demo.sql"
+	configPath      = "../../config.yaml"
+	defaultDatabase = "mysql"
 )
 
 func main() {
+	// 读取配置文件
 	sc := config.NewConfig(configPath)
 
-	// 设置默认的打开的数据库解决数据库不存在
-	sc.Mysql.Database = "mysql"
-
+	// sql文件检查
 	_, err := os.Stat(sqlPath)
 	if os.IsNotExist(err) {
 		log.Println("数据库SQL文件不存在:", err)
 		panic(err)
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", sc.Mysql.User, sc.Mysql.Password, sc.Mysql.Host, sc.Mysql.Port, sc.Mysql.Database)
+	// 创建连接
+	db := newDB(sc)
+
+	// 创建数据库
+	createDatabase(db, sc.Mysql.Database)
+	if err != nil {
+		log.Println("创建数据库失败:", err)
+		panic(err)
+	}
+
+	// 执行sql文件
+	execSqlFile(db, sqlPath)
+	if err != nil {
+		log.Println("执行sql文件失败:", err)
+		panic(err)
+	}
+
+	return
+}
+
+func newDB(sc *config.SugaredConfig) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", sc.Mysql.User, sc.Mysql.Password, sc.Mysql.Host, sc.Mysql.Port, defaultDatabase)
+
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		log.Println("数据库连接失败:", err)
 		panic(err)
 	}
+
 	db.SingularTable(true)
 	db.LogMode(true)
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
 	db.DB().SetConnMaxLifetime(59 * time.Second)
 
-	sqls, _ := ioutil.ReadFile(sqlPath)
+	return db
+}
+
+func createDatabase(db *gorm.DB, dbName string) {
+	// 创建数据库
+	err := db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;", dbName)).Error
+	if err != nil {
+		log.Println("创建数据库失败:", err)
+		panic(err)
+	}
+
+	// 使用数据库
+	err = db.Exec(fmt.Sprintf("USE %s;", dbName)).Error
+	if err != nil {
+		log.Println("使用数据库失败:", err)
+		panic(err)
+	}
+}
+
+func execSqlFile(db *gorm.DB, sqlPath string) {
+	sqls, err := ioutil.ReadFile(sqlPath)
+	if err != nil {
+		panic(err)
+	}
+
 	sqlArr := strings.Split(string(sqls), ";")
 	for _, sql := range sqlArr {
 		sql = strings.TrimSpace(sql)
@@ -59,5 +106,4 @@ func main() {
 			log.Println(sql, "\t success!")
 		}
 	}
-	return
 }
